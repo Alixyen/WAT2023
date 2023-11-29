3822254fedf4a6a62662440dd958c75f48dd196a
Alixyen <101437709+Alixyen@users.noreply.github.com>
2023-11-29 17:56:11 +0000

    fix database connection

--- basics/basics.php
+++ basics/basics.php
@@ -3,26 +3,32 @@
 echo "Alicia<br>";
 echo "C3609527<br>";
 echo "<h1> using escape characters </h1>";
-echo "most programmers say it’s better to use ‘echo’ rather than ‘print’” says who?";
+echo "Most programmers say it’s better to use ‘echo’ rather than ‘print’, says who?";
+echo "<br>";
 
 // variables.php
 $name = "David";
 $age = 12;
 echo "Hi my name is " . $name . " and I am " . $age . " years old";
+echo "<br>";
+
+
 
 // variables.php
 $name = "David";
 $age = 12;
 echo " Mi nombre es ". $name . " y tengo " . $age . " years old ";
+echo "<br>";
 
 //gettype()returns..
 echo gettype($name);
-echo '<br />';
+echo '<br>';
 //strlen() returns..
 echo strlen($name);
-echo '<br />';
+echo '<br>';
 //strtoUpper()returns..
 echo strtoUpper($name);
+echo "<br>";
 
 $num1 = 9;
 $num2 = 12;
